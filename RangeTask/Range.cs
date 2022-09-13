46878086f970ee4e92a0dfe80594a665acbccb11
Elizaveta Bayrakh <linsi30898@gmail.com>
2022-09-13 08:52:47 +0700

    Реализация дополнительных методов для задачи Range

--- RangeTask/Range.cs
+++ RangeTask/Range.cs
@@ -20,4 +20,55 @@ public class Range
     {
         return number >= From && number <= To;
     }
+
+    public bool IsCrossing(Range b)
+    {
+        return From <= b.To || To >= b.From;
+    }
+
+    public Range CrossIntervals(Range b)
+    {
+        if (IsCrossing(b))
+        {
+            return new Range(b.From, To);
+        }
+
+        return null;
+    }
+
+    public Range[] CombineIntervals(Range b)
+    {
+        if (IsCrossing(b))
+        {
+            return new[] { new Range(From, b.To) };
+        }
+
+        return new[] { this, b };
+    }
+
+    public Range[] SubtractIntervals(Range b)
+    {
+        if (From >= b.From && To <= b.To)
+        {
+            return Array.Empty<Range>();
+        }
+
+        if (From >= b.From && To > b.To && IsCrossing(b))
+        {
+            return new[] { new Range(b.To, To) };
+        }
+
+        if (From < b.From && To <= b.To && IsCrossing(b))
+        {
+            return new[] { new Range(From, b.From) };
+        }
+
+        if (From < b.From && To > b.To)
+        {
+            return new[] { new Range(From, b.From), new Range(b.To, To) };
+        }
+
+        return new[] { this };
+    }
+
 }
